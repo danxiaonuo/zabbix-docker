4e2174658c1adb46acae7947079da2e983f5405b
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-01-12 15:58:46 +0800

    init

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
