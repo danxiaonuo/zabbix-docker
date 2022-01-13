f9e0b80aeaa12aca2a44fc32d83acbb9c9c17faa
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-01-13 10:12:57 +0800

    更新服务端

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
 docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
