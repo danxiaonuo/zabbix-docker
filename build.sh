fb26e4dabdf591c5d296d22c85d6ca71b0cfb0aa
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-01-21 15:11:30 +0800

    更新web

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
-docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
-docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
-docker build . -t danxiaonuo/zabbix-snmptraps:latest -f snmptraps/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-snmptraps:latest -f snmptraps/Dockerfile --force-rm --no-cache --network=host
 docker build . -t danxiaonuo/zabbix-web-nginx:latest -f web-nginx/Dockerfile --force-rm --no-cache --network=host
