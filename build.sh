b67dfca43577668d80f3071f404bcb084881b01a
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-02-09 16:30:06 +0800

    更新

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-snmptraps:latest -f snmptraps/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-web-nginx:latest -f web-nginx/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-proxy:latest -f zabbix-proxy/Dockerfile --force-rm --no-cache --network=host
-# docker build . -t danxiaonuo/zabbix-agent2:latest -f zabbix-agent2/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-build-base:latest -f build-base/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-web-service:latest -f web-service/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-web-nginx:latest -f web-nginx/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-proxy:latest -f zabbix-proxy/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-agent2:latest -f zabbix-agent2/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-snmptraps:latest -f snmptraps/Dockerfile --force-rm --no-cache --network=host
+docker build . -t danxiaonuo/zabbix-java-gateway:latest -f java-gateway/Dockerfile --force-rm --no-cache --network=host
