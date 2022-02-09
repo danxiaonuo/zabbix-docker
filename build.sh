f844fdb2d171c9579a792db6fe950973596b56d7
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-02-09 15:02:15 +0800

    更新客户端

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 # docker build . -t danxiaonuo/zabbix-build-mysql:latest -f build-mysql/Dockerfile --force-rm --no-cache --network=host
 # docker build . -t danxiaonuo/zabbix-server:latest -f zabbix-server/Dockerfile --force-rm --no-cache --network=host
 # docker build . -t danxiaonuo/zabbix-snmptraps:latest -f snmptraps/Dockerfile --force-rm --no-cache --network=host
-docker build . -t danxiaonuo/zabbix-web-nginx:latest -f web-nginx/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-web-nginx:latest -f web-nginx/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-proxy:latest -f zabbix-proxy/Dockerfile --force-rm --no-cache --network=host
+# docker build . -t danxiaonuo/zabbix-agent2:latest -f zabbix-agent2/Dockerfile --force-rm --no-cache --network=host
