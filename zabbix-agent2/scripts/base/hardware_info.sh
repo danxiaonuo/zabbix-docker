ffd9711bfda6a8656327e2a02234b066f460b1a1
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-03-28 15:59:04 +0800

    Update hardware_info.sh

--- zabbix-agent2/scripts/base/hardware_info.sh
+++ zabbix-agent2/scripts/base/hardware_info.sh
@@ -12,37 +12,37 @@ if [ "${processNum}" -gt "2" ]; then
 fi
 
 # 获取服务器厂家
-${zbx_sender} -c ${CONFIGFILE} -k "system.manufacturer" -o $(sudo dmidecode -s system-manufacturer | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "system.manufacturer" -o "$(sudo dmidecode -s system-manufacturer | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取服务器类型
-${zbx_sender} -c ${CONFIGFILE} -k "system.product.name" -o $(sudo dmidecode -s system-product-name | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "system.product.name" -o "$(sudo dmidecode -s system-product-name | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取服务器SN号
-${zbx_sender} -c ${CONFIGFILE} -k "system.serial.number" -o $(sudo dmidecode -s system-serial-number | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "system.serial.number" -o "$(sudo dmidecode -s system-serial-number | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取服务器UUID
-${zbx_sender} -c ${CONFIGFILE} -k "system.uuid" -o $(sudo dmidecode -s system-uuid | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "system.uuid" -o "$(sudo dmidecode -s system-uuid | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取主板生产厂家
-${zbx_sender} -c ${CONFIGFILE} -k "baseboard.manufacturer" -o $(sudo dmidecode -s baseboard-manufacturer | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "baseboard.manufacturer" -o "$(sudo dmidecode -s baseboard-manufacturer | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取主板产品名称
-${zbx_sender} -c ${CONFIGFILE} -k "baseboard.product.name" -o $(sudo dmidecode -s baseboard-product-name | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "baseboard.product.name" -o "$(sudo dmidecode -s baseboard-product-name | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取主板版本
-${zbx_sender} -c ${CONFIGFILE} -k "baseboard.version" -o $(sudo dmidecode -s baseboard-version | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "baseboard.version" -o "$(sudo dmidecode -s baseboard-version | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取主板序列号
-${zbx_sender} -c ${CONFIGFILE} -k "baseboard.serial.number" -o $(sudo dmidecode -s baseboard-serial-number | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "baseboard.serial.number" -o "$(sudo dmidecode -s baseboard-serial-number | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取操作系统名称
-${zbx_sender} -c ${CONFIGFILE} -k "os.name" -o $(python2 -c "import platform;print(platform.linux_distribution()[0])" | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "os.name" -o "$(python2 -c "import platform;print(platform.linux_distribution()[0])" | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取操作系统类型
-${zbx_sender} -c ${CONFIGFILE} -k "os.type" -o $(uname -o | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "os.type" -o "$(uname -o | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取操作系统版本
-${zbx_sender} -c ${CONFIGFILE} -k "os.version" -o $(python2 -c "import platform;print(platform.linux_distribution()[1])" | sed "/^#/d") >/dev/null 2>&1
+${zbx_sender} -c ${CONFIGFILE} -k "os.version" -o "$(python2 -c "import platform;print(platform.linux_distribution()[1])" | sed "/^#/d")" >/dev/null 2>&1
 
 # 获取CPU型号
 ${zbx_sender} -c ${CONFIGFILE} -k "cpu.module" -o "$(cat /proc/cpuinfo | awk -F': '+ '/model name/{print $2}' | uniq | sed "/^#/d")" >/dev/null 2>&1
