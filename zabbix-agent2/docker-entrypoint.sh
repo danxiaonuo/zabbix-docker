00ce2c7d47e156591314934247ca76b6750be027
danxiaonuo <danxiaonuo@danxiaonuo.me>
2024-06-21 16:41:55 +0800

    Update docker-entrypoint.sh

--- zabbix-agent2/docker-entrypoint.sh
+++ zabbix-agent2/docker-entrypoint.sh
@@ -133,7 +133,7 @@ prepare_zbx_agent_config() {
     update_config_var $ZBX_AGENT_CONFIG "LogFileSize"
     update_config_var $ZBX_AGENT_CONFIG "DebugLevel" "${ZBX_DEBUGLEVEL}"
     update_config_var $ZBX_AGENT_CONFIG "SourceIP"
-	  update_config_var $ZBX_AGENT_CONFIG "Plugins.SystemRun.LogRemoteCommands" "${ZBX_LOGREMOTECOMMANDS}"
+    update_config_var $ZBX_AGENT_CONFIG "Plugins.SystemRun.LogRemoteCommands" "${ZBX_LOGREMOTECOMMANDS}"
 
     : ${ZBX_PASSIVE_ALLOW:="true"}
     if [ "${ZBX_PASSIVE_ALLOW,,}" == "true" ]; then
@@ -178,7 +178,7 @@ prepare_zbx_agent_config() {
     update_config_var $ZBX_AGENT_CONFIG "RefreshActiveChecks" "${ZBX_REFRESHACTIVECHECKS}"
     update_config_var $ZBX_AGENT_CONFIG "BufferSend" "${ZBX_BUFFERSEND}"
     update_config_var $ZBX_AGENT_CONFIG "BufferSize" "${ZBX_BUFFERSIZE}"
-	  update_config_var $ZBX_AGENT_CONFIG "Plugins.Log.MaxLinesPerSecond" "${ZBX_MAXLINESPERSECOND}"
+    update_config_var $ZBX_AGENT_CONFIG "Plugins.Log.MaxLinesPerSecond" "${ZBX_MAXLINESPERSECOND}"
     # Please use include to enable Alias feature
     # update_config_multiple_var $ZBX_AGENT_CONFIG "Alias" ${ZBX_ALIAS}
     update_config_var $ZBX_AGENT_CONFIG "Timeout" "${ZBX_TIMEOUT}"
@@ -203,7 +203,7 @@ prepare_zbx_agent_config() {
 prepare_zbx_agent_plugin_config() {
     echo "** Preparing Zabbix agent plugin configuration files"
 
-	  sed -i "/Include=.*/i\Include=${ZABBIX_ETC_DIR}/zabbix_agent2.d/plugins.d/*.conf" ${ZBX_AGENT_CONFIG}
+    sed -i "/Include=.*/i\Include=${ZABBIX_ETC_DIR}/zabbix_agent2.d/plugins.d/*.conf" ${ZBX_AGENT_CONFIG}
     # update_config_var "${ZABBIX_ETC_DIR}/zabbix_agent2.d/plugins.d/mongodb.conf" "Plugins.MongoDB.System.Path" "/usr/sbin/zabbix-agent2-plugin/mongodb"
     # update_config_var "${ZABBIX_ETC_DIR}/zabbix_agent2.d/plugins.d/postgresql.conf" "Plugins.PostgreSQL.System.Path" "/usr/sbin/zabbix-agent2-plugin/postgresql"
     update_config_var "${ZABBIX_ETC_DIR}/zabbix_agent2.d/plugins.d/mssql.conf" "Plugins.MSSQL.System.Path" "/usr/sbin/zabbix-agent2-plugin/mssql"
